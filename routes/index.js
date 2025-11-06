f0bf62d50f49026a31cd9c4efae96bbe4f1fa3b7
ycerv <ycerv2@illinois.edu>
2025-11-05 21:20:00 -0600

    mp3 implementation try 2

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@
  */
 module.exports = function (app, router) {
     app.use('/api', require('./home.js')(router));
-    app.use('/tasks', require('./tasks.js')(router));
-    app.use('/users', require('./users.js')(router));
+    app.use('/api', require('./tasks.js')(router));
+    app.use('/api', require('./users.js')(router));
 };
